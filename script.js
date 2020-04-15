375984178935d8dccb3528ff231a40f18a9f9289
miyakooti <60727025+miyakooti@users.noreply.github.com>
2020-04-15 11:03:31 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+$(function(){
+  $('#go').click(function(){
+    $('#jquery-box').css('display', 'block');
+    $(this).css('display', 'none');
+  });
+
+  $('#exit').click(function(){
+    $('#jquery-box').css('display', 'none');
+    $('#go').css('display', 'block');
+  })
+
+
+
+
+}
+)
